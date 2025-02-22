e820d38bd605f360eb587a2dc3ecf7609d92880f
Mohamed Majed <malmasdi@jahez.net>
2025-02-22 17:18:46 +0200

    Add project files.

---
+++ Basic/Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+var queueName = "test";
+
+await channel.QueueDeclareAsync(queue: queueName,
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.ReceivedAsync += (model, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Messaged Received: {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.ReadKey();
---
+++ Basic/Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+var queueName = "test";
+
+await channel.QueueDeclareAsync(queue: queueName,
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+var message = $"Hello from producer {DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss")}";
+var messageEncoded = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty,
+                                routingKey: queueName,
+                                body: messageEncoded);
+
+Console.WriteLine($"Messaged Published: {message}");
